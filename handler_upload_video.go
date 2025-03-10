1bce204ebc9a70f8fcc858810f324f5cfec0c3d5
Mads Bach Villadsen <mvilladsen@pm.me>
2025-03-10 11:02:26 -0400

    Save videos by aspect ratio and with fast start

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/constraints"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -70,18 +76,41 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	defer os.Remove("tubely-upload.mp4")
 	defer tmpfile.Close()
 
+	// Write uploaded video to disk temporarily
 	_, err = io.Copy(tmpfile, formFile)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong while writing file", err)
 		return
 	}
 
+	processedPath, err := processVideoForFastStart(tmpfile.Name())
+	processedFile := tmpfile
+	if err != nil {
+		log.Printf("Failed to process for fast start, continuing with original: %s\n%s\n", tmpfile.Name(), err)
+	} else {
+		processedFile, err = os.Open(processedPath)
+		if err != nil {
+			log.Printf("Failed to open fast start processed file, continuing with original: %s", err)
+		} else {
+			defer processedFile.Close()
+			tmpfile = processedFile
+		}
+	}
+
+	// Reset cursor to copy tmpfile to s3
 	_, err = tmpfile.Seek(0, io.SeekStart)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Something went wrong while writing file", err)
 		return
 	}
-	key := fmt.Sprintf("%s.mp4", videoID)
+
+	prefix, err := getVideoAspectRatio(tmpfile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something went wrong while writing file", err)
+		return
+	}
+
+	key := fmt.Sprintf("%s/%s.mp4", prefix, videoID)
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
 		Key:         &key,
@@ -101,4 +130,58 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	respondWithJSON(w, http.StatusOK, videoMetaData)
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	cmd := fmt.Sprintf("ffprobe -v error -print_format json -show_streams %s | jq '.streams.[0].height'", filePath)
+	s := exec.Command("zsh", "-c", cmd)
+	out, err := s.Output()
+	fmt.Println(strings.TrimSpace(string(out[:])), err)
+	if err != nil {
+		return "", err
+	}
+	height, err := strconv.Atoi(strings.TrimSpace(string(out[:])))
+	if err != nil {
+		return "", err
+	}
+
+	cmd = fmt.Sprintf("ffprobe -v error -print_format json -show_streams %s | jq '.streams.[0].width'", filePath)
+	s = exec.Command("zsh", "-c", cmd)
+	out, err = s.Output()
+	fmt.Println(strings.TrimSpace(string(out[:])), err)
+	if err != nil {
+		return "", err
+	}
+	width, err := strconv.Atoi(strings.TrimSpace(string(out[:])))
+	if err != nil {
+		return "", err
+	}
+
+	if Abs(width*9-height*16) < 16 {
+		// landscape: width/height = 16/9
+		return "landscape", nil
+	} else if Abs(height*9-width*16) < 16 {
+		// portrait: height/width = 16/9
+		return "portrait", nil
+	} else {
+		return "other", nil
+	}
+}
+
+func Abs[T constraints.Integer](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func processVideoForFastStart(filePath string) (string, error) {
+	processing := filePath + ".processing"
+
+	cmd := exec.Command("ffmpeg", "-i", filePath, "-c", "copy", "-movflags", "faststart", "-f", "mp4", processing)
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return processing, nil
 }
